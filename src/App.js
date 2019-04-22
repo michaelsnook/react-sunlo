b014e6c5ea4c08438e0a797fdf020b77badcbe99
Michael Snook <snook@the-open.net>
2019-04-22 11:19:24 +0530

    Add bootstrap boilerplate cards

--- src/App.js
+++ src/App.js
@@ -5,21 +5,37 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="btn btn-lg btn-primary"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col">
+            <div className="card-deck">
+              <div className="card">
+                <img className="card-img-top" src="https://placehold.it/362x200" alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div className="card">
+                <img className="card-img-top" src="https://placehold.it/362x200" alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div className="card">
+                <img className="card-img-top" src="https://placehold.it/362x200" alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
