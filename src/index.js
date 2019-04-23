3205c4b8a444f9ac4acc4fb1d1c3dd6c15e84e2c
Michael Snook <snook@the-open.net>
2019-04-23 13:50:17 +0530

    Remove unused index.css (but leave placeholder file)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -8,6 +7,8 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
+// our own styles
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
