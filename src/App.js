e77591b555527dc01fa36cc5aeeecaa43ed6d043
Michael Snook <snook@the-open.net>
2019-04-26 17:55:13 +0530

    Add card list and connect to new base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import MovieList from './MovieList';
-import PostList from './PostList';
+import CardList from './CardList';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
-        <PostList />
-        <MovieList />
+        <CardList />
         <Footer />
       </Fragment>
     );
--- src/Settings.js
+++ src/Settings.js
@@ -1,4 +1,4 @@
 export default {
-  API_KEY: '', // 'keyasdf12345
-  API_URL: '', // 'https://api.airtable.com/v0/appqwerty67890/'
+  API_KEY: 'key68P1NYs0twQGks', // 'keyasdf12345
+  API_URL: 'https://api.airtable.com/v0/appR6XwqnBeu0ekX1/', // 'https://api.airtable.com/v0/appqwerty67890/'
 };
