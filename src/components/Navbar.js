07050402cabbde3bf8c228108d81c6ceeb2f99da
Michael Snook <snook@the-open.net>
2019-04-27 12:09:45 +0530

    Bring some color back

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Route, Link } from 'react-router-dom'
 
 const Navbar = () => (
-  <nav className="bg-dark text-white">
+  <nav className="bg-primary text-white">
     <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark">
         <Link to={`/`} className="navbar-brand">Sunlo</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -10,7 +10,7 @@ class Welcome extends Component {
 
   render() {
     return(
-      <div className="container py-5">
+      <div className="container py-5 bg-primary text-light">
         <h1>Sunlo is a social<br />language learning app</h1>
         <br />
         <p>Create your own flash cards, or pick from a crowd-sourced pool.</p>
@@ -19,7 +19,7 @@ class Welcome extends Component {
         help you learn new words and phrases that are useful from day one.</p>
         <p>(This is not a company, just an app. It's open source and free to use.
           If you like to code and want to help with the app,
-          <a href="https://github.com/michaelsnook/sunlo"> come have a look.</a>)</p>
+          <strong> <a className="text-white border-bottom" href="https://github.com/michaelsnook/react-sunlo">come have a look.</a></strong>)</p>
       </div>
     );
   }
