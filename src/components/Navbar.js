171f142c2091bc2cbdf1049554924ec42c900d4f
Michael Snook <snook@the-open.net>
2019-04-27 16:25:17 +0530

    Make Navbar into a proper Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Route, Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav className="bg-primary text-white">
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <Link to={`/`} className="navbar-brand">Sunlo</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link to={`/profile`} className="nav-item nav-link active">
-              <i className="fas fa-user-circle col-2"></i> Profile
-            </Link>
-            <Link to={`/deck/Hindi`} className="nav-item nav-link active">
-              <i className="fas fa-database col-2"></i> Your Hindi deck
-            </Link>
-            <Link to={`/browse/Hindi`} className="nav-item nav-link active">
-              <i className="fas fa-database col-2"></i> Browe Hindi phrases
-            </Link>
-          </div>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  render() {
+    return (
+      <nav className="bg-primary text-white">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-dark">
+            <Link to={`/`} className="navbar-brand">Sunlo</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <Link to={`/profile`} className="nav-item nav-link active">
+                  <i className="fas fa-user-circle col-2"></i> Profile
+                </Link>
+                <Link to={`/deck/Hindi`} className="nav-item nav-link active">
+                  <i className="fas fa-database col-2"></i> Your Hindi deck
+                </Link>
+                <Link to={`/browse/Hindi`} className="nav-item nav-link active">
+                  <i className="fas fa-database col-2"></i> Browe Hindi phrases
+                </Link>
+              </div>
+            </div>
+          </nav>
         </div>
       </nav>
-    </div>
-  </nav>
-);
+    );
+  }
+}
 
 export default Navbar;
