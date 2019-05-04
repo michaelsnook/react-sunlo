d80281d6a6e3dc0233b94ca416d4e82ce1c8e317
Michael Snook <snook@the-open.net>
2019-05-04 13:30:29 +0530

    Turn a Deck's state.phrases into an object with keys

--- src/screens/deck.js
+++ src/screens/deck.js
@@ -7,7 +7,7 @@ class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      phrases: []
+      phrases: {}
     };
   }
 
@@ -16,10 +16,21 @@ class Deck extends Component {
         + '&view=deck_' + this.props.language.toLowerCase())
       .then((resp) => resp.json())
       .then(data => {
-        this.setState({ phrases: data.records })
+        let phrases = {};
+        data.records.map(p => { phrases[p.id] = p })
+        this.setState({ phrases: phrases })
       }).catch(err => {
         // Error 🙁
       });
+  }
+
+  drawPhrases() {
+    let items = [];
+    for (let key in this.state.phrases) {
+      const phrase = this.state.phrases[key];
+      items.push(<DeckItem key={`deck-item-${key}`} phrase={phrase} />)
+    }
+    return items;
   }
 
   render() {
@@ -27,11 +38,7 @@ class Deck extends Component {
       <div className="container p-3">
         <h2>Your {this.props.language} Deck</h2>
         <div className="row p-3">
-        {this.state.phrases.sort((a, b) => {
-          return (a.fields.status < b.fields.status) ? -1 : 1
-        }).map(phrase =>
-          <DeckItem key={phrase.id} phrase={phrase} />
-        )}
+        {this.drawPhrases()}
         </div>
       </div>
     );
